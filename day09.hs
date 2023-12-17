b89219e387ff00005c1011fe09ce40e97ed0cc29
Amneesh Singh <natto@weirdnatto.in>
2023-12-17 17:33:49 +0530

    day 9: remove imports
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- day09.hs
+++ day09.hs
@@ -1,17 +1,13 @@
-import Data.Either (rights)
-import Data.Map qualified as M
 import Data.Text qualified as T
-import Data.Text.Read qualified as TR
 import Lib (readFile', tRead)
-import Text.Parsec
 
 main :: IO ()
 main = do
   input <- map (map tRead . T.words) . T.lines <$> readFile' "day09.in"
-  
+
   putStr "Q1: "
   print . sum $ map (sum . map last . extrap) input
-  
+
   putStr "Q2: "
   print . sum $ map (sum . map last . extrap . reverse) input
 
